fbd5199ed97625b84282fd6c1634eb56aae79920
TaeeunKim <xodms0309@naver.com>
2023-07-28 12:16:43 +0900

    build: 빌드 테스트

--- src/components/SpinButton.tsx
+++ src/components/SpinButton.tsx
@@ -1,16 +1,20 @@
-import React, { useState, MouseEvent } from "react";
-import "./SpinButton.css";
+import React, { useState, MouseEvent } from 'react';
+import './SpinButton.css';
 
 const SpinButton: React.FC = () => {
   const [count, setCount] = useState<number>(0);
   const [isTooltipVisible, setIsTooltipVisible] = useState<boolean>(false);
 
+  const [ariaText, setAriaText] = useState('');
+
   const increment = () => {
     setCount((prevCount) => prevCount + 1);
+    setAriaText('성인 승객 추가');
   };
 
   const decrement = () => {
     setCount((prevCount) => prevCount - 1);
+    setAriaText('성인 승객 감소');
   };
 
   const toggleTooltip = (event: MouseEvent<HTMLDivElement>) => {
@@ -18,33 +22,50 @@ const SpinButton: React.FC = () => {
   };
 
   return (
-    <section className="spinButtonContainer">
+    <section className='spinButtonContainer'>
       <div>
         <h1>승객 선택</h1>
-        <div className="spinButtonLabel">
+        <div className='spinButtonLabel'>
           <label>성인</label>
           <div
-            className="helpIcon"
+            className='helpIcon'
             onMouseEnter={toggleTooltip}
             onMouseLeave={toggleTooltip}
           >
             ?
             {isTooltipVisible && (
-              <span className="tooltip">최대 인원수는 3명까지 가능합니다</span>
+              <span className='tooltip'>최대 인원수는 3명까지 가능합니다</span>
             )}
           </div>
         </div>
-        <button onClick={decrement} className="spinButton">
+        <button
+          onClick={decrement}
+          className='spinButton'
+          aria-label='성인 탑승자 한명 줄이기'
+          disabled={count === 0}
+        >
           -
         </button>
+        <p aria-live='assertive' aria-hidden={false}>
+          {ariaText} {count}
+        </p>
         <input
-          type="text"
-          role="spinbutton"
+          id='spinButtonInput'
+          type='number'
+          role='spinbutton'
           readOnly
-          className="spinButtonInput"
+          className='spinButtonInput'
           value={count}
+          min={1}
+          max={3}
+          aria-label='텍스트 숫자만 수정'
         />
-        <button onClick={increment} className="spinButton">
+        <button
+          onClick={increment}
+          className='spinButton'
+          aria-label='성인 탑승자 한명 늘리기'
+          disabled={count === 3}
+        >
           +
         </button>
       </div>
